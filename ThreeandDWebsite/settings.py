ad560b39d1273a7cd4aed047696d9ad960e35d8d
benrogers31 <benrogers@live.ca>
2020-06-05 16:22:04 +0000

    added ip and static root to settings.py

--- ThreeandDWebsite/settings.py
+++ ThreeandDWebsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY  = os.environ.get('THREE_AND_D_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.105.5.123']
 
 
 # Application definition
@@ -126,6 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 
 
